0475de378a428d84aa34a72be9d33b4ecb62dd82
Alexander Stein <code+al@il5.in>
2014-01-28 20:33:42 +0300

    password_store.py: Rename for less Python import hassle

---
+++ password_store.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# Simple wrapper for offlineimap to grab password stored in Pass - The Standard
+# Unix Password Manager available at: http://zx2c4.com/projects/password-store
+#
+# Passwords are called by running the command 'pass mypassword', this just
+# wraps that with Python as you cannot seem to put shell commands in
+# .offlineimaprc file.
+
+import subprocess
+
+def pass_get(secret_name):
+  return(subprocess.check_output(["pass", secret_name])).rstrip('\n')
